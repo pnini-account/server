9abe9fc69e6ffc5034cc468967c288146cf38b2b
פנינה פרידמן <326036654@mbydomain>
2023-02-27 16:09:35 +0200

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,57 +1,61 @@
 const db = require('../models/index')
-const bcrypt= require('bcrypt')
-const jwt= require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
- const User = db.user;
+const User = db.user;
 // const bcrypt= require('bcrypt');
 
 const login = async (req, res) => {
 
-    const {id, password} = req.body;
+    const { id, password } = req.body;
     if (!id || !password) {
-        return res.status(400).json({ message: 'All fields are required'
+        return res.status(400).json({
+            message: 'All fields are required'
         })
     }
-    const foundUser = await User.findOne({where:{id:id}})
+    const foundUser = await User.findOne({ where: { id: id } })
     if (!foundUser || !foundUser.active) {
         return res.status(401).json({ message: 'Unauthorized' })
-        }
+    }
     const match = await bcrypt.compare(password, foundUser.password)
     if (!match) return res.status(401).json({ message: 'Unauthorized' })
     res.send("Logged In")
 
-    const userInfo= {id:foundUser.id,name:foundUser.name,
-        email:foundUser.email, warningsId:foundUser.warningsId,
-        filesId:foundUser.filesId,foldersId:foundUser.foldersId,
-        categoriesId:foundUser.categoriesId} 
+    const userInfo = {
+        id: foundUser.id, name: foundUser.name,
+        email: foundUser.email, warningsId: foundUser.warningsId,
+        filesId: foundUser.filesId, foldersId: foundUser.foldersId,
+        categoriesId: foundUser.categoriesId
+    }
 
-    const accessToken = jwt.sign(userInfo,"לערבול סיסמא")
-    
+    //const accessToken = jwt.sign(userInfo,"לערבול סיסמא")
+    const accessToken = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET)
 
-
+    res.json({ accessToken: accessToken })
 }
 
 const register = async (req, res) => {
-    const {name, email, password,  warningsId, filesId} = req.body
-if (!name || !email || !password || !warningsId || !filesId) {// Confirm data
-    return res.status(400).json({ message: 'All fields are required' })
-}
-   
-const duplicate = await User.findOne({where:{name:name}})
-if(duplicate){
-return res.status(409).json({message:"Duplicate username"})
-}
-const hashedPwd = await bcrypt.hash(password, 10)
+    const { name, email, password, warningsId, filesId } = req.body
+    if (!name || !email || !password || !warningsId || !filesId) {// Confirm data
+        return res.status(400).json({ message: 'All fields are required' })
+    }
 
-const userObject = {name,email,password,warningsId, filesId:hashedPwd}
-const user = await User.create(userObject)
-if (user) { // Created
-return res.status(201).json({message:`New user ${user.userName} created`
-})
-} else {
-return res.status(400).json({ message: 'Invalid user data received' })
-}
+    const duplicate = await User.findOne({ where: { name: name } })
+    if (duplicate) {
+        return res.status(409).json({ message: "Duplicate username" })
+    }
+    const hashedPwd = await bcrypt.hash(password, 10)
+
+    const userObject = { name, email, password, warningsId, filesId: hashedPwd }
+    const user = await User.create(userObject)
+    if (user) { // Created
+        return res.status(201).json({
+            message: `New user ${user.userName} created`
+        })
+    } else {
+        return res.status(400).json({ message: 'Invalid user data received' })
+    }
 
 }
 
-module.exports = {login, register}
+module.exports = { login, register }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const authController = require("../controllers/authController")
+const verifyJWT = require("../middleware/verifyJWT")
+
 router.post('/register', authController.register)
-router.post('/login', authController.login)
+//router.post('/login', authController.login)
+router.route('/login')
+    .post(verifyJWT, authController.login)
+
 module.exports = router
